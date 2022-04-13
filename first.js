a09a39d6630ef6c2c7087eec97f9a00b193fca1f
LauraBenavidesdlrv <ldbenavidesr@unal.edu.co>
2022-04-13 09:57:05 -0500

    uploading 3, 4 and 5

--- first.js
+++ first.js
@@ -17,3 +17,75 @@ function prom(notas){
 }
 prom([2,2.5,5,4.2,2.1])
 
+/* 3) solicitar respuestas correctas, incorrectas y en blanco de un postulante, teniendo en cuenta que:
+    correcta -> 4 puntos
+    incorrecta -> -1 puntos
+    En blanco -> 0 puntos
+*/
+function puntaje(correctas, incorrectas, blanco){
+    let puntajeFinal = (correctas * 4) + (incorrectas * -1) + (blanco * 0)
+    console.log(puntajeFinal)
+}
+puntaje(6,3,3)
+
+/* 3.1) solicitar respuestas correctas, incorrectas y en blanco de varios postulantes, teniendo en cuenta que (lo voy a hacer con tope de 12 preguntas):
+    correcta -> 4 puntos
+    incorrecta -> -1 puntos
+    En blanco -> 0 puntos
+*/
+function puntaje2(listaPostulantes){
+    let resultados = []
+    for(let i = 0; i < listaPostulantes.length; i++){
+        let nombre = listaPostulantes[i].nombre;
+        console.log(nombre)
+        let puntajeTotal = (listaPostulantes[i].correctas * 4) + (listaPostulantes[i].incorrectas * -1)+(listaPostulantes[i].blanco * 0)
+        resultados.push("Nombre del postulante: " + nombre +" -> " +"Puntaje total =  " + puntajeTotal)
+        console.log(resultados)
+    }
+}
+puntaje2([
+    {nombre:"Adriana", correctas:5, incorrectas:5, blanco:2,}, 
+    {nombre:"Pedro", correctas:3, incorrectas:5, blanco:4 }, 
+    {nombre:"Carlos", correctas:8, incorrectas:4, blanco:0 },
+    {nombre:"Laura", correctas:12, incorrectas:0, blanco:0 },
+    {nombre:"Juliana", correctas:5, incorrectas:0, blanco:7 }
+])
+
+/* 4) calcular puntaje total de un equipo en el torneo de apertura teniendo en cuenta que:
+    Partido ganados -> 3 puntos
+    partido empatado -> 1 puntos
+    partido perdido -> 0 puntos
+*/
+//Es igual que el anterior
+function puntajeTorneoApertura (listaEquipos){
+    let datosFinales = []
+    for(let i = 0; i<listaEquipos.length; i++){
+        let equipo = listaEquipos[i].Equipo;
+        let puntajeTotal = (listaEquipos[i].ganados * 3) + (listaEquipos[i].perdidos * 0) + (listaEquipos[i].empatados* 1)
+        datosFinales.push("Equipo: " + equipo + " -> Puntaje de torneo = " + puntajeTotal)
+        console.log(datosFinales)
+    }
+}
+puntajeTorneoApertura([
+    {Equipo:"Colombia", ganados:1, perdidos:1, empatados:10}, 
+    {Equipo:"Peru", ganados:5, perdidos:5, empatados:1}, 
+    {Equipo:"Argentina", ganados:9, perdidos:0, empatados:2 },
+    {Equipo:"Chile", ganados:7, perdidos:2, empatados:2 },
+    {Equipo:"Brasil", ganados:10, perdidos:0, empatados:1 }
+])
+
+// 5) Se requiere un algoritmo para elaborara la plantilla de un empleado, teniendo en cuenta las horas laboradas al mes y de la tarifa por hora.
+
+function calcSueldo(horas, tarifa){
+    let sueldo = horas * tarifa;
+    console.log("Salario " + "$" + sueldo )
+}
+ calcSueldo(160, 3000)
+
+
+
+
+
+
+
+
